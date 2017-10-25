ab077fe0577557362eea2582bca1a5ac49c8e2ba
RaiderSoap <info@chivalrystudio.com>
2017-10-25 16:58:06 -0700

    Add template directory

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import GameObject from './donjon_objects/game_objects';
+import Rigidbody from './donjon_components/comp_rigidbody';
+import Collider from './donjon_components/comp_collider';
+import Transform from './donjon_components/comp_transform';
+
+export {
+   GameObject,
+   Transform,
+   Rigidbody,
+   Collider,
+};
