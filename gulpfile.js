69202ac6211bbdce575f508b5545eed22f42e7a4
RaiderSoap <info@chivalrystudio.com>
2017-10-25 16:21:44 -0700

    Auto test after build

--- gulpfile.js
+++ gulpfile.js
@@ -1,60 +1,78 @@
 var gulp = require('gulp');
 var gulpBabel = require("gulp-babel");
 var gulpConcat = require('gulp-concat');
+var gulpHeader = require('gulp-header');
+var gulpRun = require('gulp-run');
+var gulpJsdoc = require('gulp-jsdoc3');
 
 var merge = require('merge-stream');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
-
 var fs = require('fs');
 var path = require('path');
 
-var jsdoc = require('gulp-jsdoc3');
+var runSequence = require('run-sequence');
 
+//headers
+var license = '/*\n ' + fs.readFileSync('LICENSE') + '*/\n';
+var fileTitle = "\n//=======================================================" +
+  "======================================================//\n\n";
 
-gulp.task('default', ['build', 'browserify']);
+//default task
+gulp.task('default', ['build']);
 
+//directory
 var dirs = {
   src: "src",
   dest: "js"
 };
 
-/**
- * @param dir
- * @returns {Array}
- */
-function getFolders(dir) {
-  return fs.readdirSync(dir)
-    .filter(function (file) {
-      return fs.statSync(path.join(dir, file)).isDirectory();
-    });
-}
+gulp.task('build', function () {
+  runSequence('compile', 'build:donjon', function () {
+    gulpRun('npm run test').exec();
+  });
+});
+
 
 /**
  * combine all files into one js file and then compile es6 into es5
  */
-gulp.task('build', function () {
-  return gulp.src('src/**/**.js')
+gulp.task('compile', function () {
+  return gulp.src(['src/**/**.js', '!src/libs/**'])
     .pipe(gulpBabel())
+    .pipe(gulpHeader(fileTitle))
     .pipe(gulp.dest(dirs.dest));
 });
 
 /**
- * browserify app to client
+ * browserify and export public classes.
  */
-gulp.task('browserify', function () {
-  return browserify([
-    'js/main.js'
-  ]).bundle()
-    .pipe(source('bundle.js'))
+gulp.task('build:donjon', function () {
+  var b = browserify('js/index.js', {
+    standalone: 'Donjon'
+  }).bundle()
+    .pipe(source('donjon.js'))
+    .pipe(gulpHeader(license))
     .pipe(gulp.dest('./build/'));
+
+  var c = gulp.src('src/libs/*.js')
+    .pipe(gulp.dest('./build/libs'));
+
+  return merge(b, c);
 });
 
 /**
- *
+ * generate jsdoc
  */
 gulp.task('doc', function (cb) {
   gulp.src(['./js/**/*.js'], {read: false})
-    .pipe(jsdoc(cb))
+    .pipe(gulpJsdoc(cb))
     .pipe(gulp.dest('./docs'));
+});
+
+/**
+ * auto test
+ */
+gulp.task('test', function () {
+  gulpRun('npm run test').exec();
 });
--- tests/test_transform.js
+++ tests/test_transform.js
@@ -1,30 +1,30 @@
 var assert = require('assert');
+var Victor = require('victor');
 
-/* Dummy method to import Test class */
-var Transform = require('../js/donjon_components/comp_transform').default;
-var GameObject = require('../js/donjon_objects/game_objects').default;
-var Victor = require('../js/libs/victor');
+/**
+ * @type {{GameObject, Transform}}
+ */
+var Donjon = require('../build/donjon');
 
-
-/* create dummy test objects */
-var obj = new GameObject("asd");
-var t1 = new Transform(obj);
 
 /**
  * start describing
  */
-describe('Transform', function() {
+describe('Transform', function () {
 
+  /* create dummy test objects */
+  var obj = new Donjon.GameObject("asd");
+  var t1 = new Donjon.Transform(obj);
 
-  describe('#translate()', function() {
+  describe('#translate()', function () {
 
-    t1.translate(new Victor(5,5));
+    t1.translate(new Victor(5, 5));
 
-    it('should change position x to 5',function () {
-      assert.equal(t1.position.x,5);
+    it('should change position x to 5', function () {
+      assert.equal(t1.position.x, 5);
     });
-    it('should change position y to 5',function () {
-      assert.equal(t1.position.x,5);
+    it('should change position y to 5', function () {
+      assert.equal(t1.position.x, 5);
     });
 
   });
